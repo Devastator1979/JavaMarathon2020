7624b401f14480a6f35797b3188ee8665eb5663c
devastator1979 <brestdevastator@gmail.com>
2020-12-13 23:55:35 +0300

    Day 2 Task 3

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,9 +1,25 @@
 package day2;
+            /* реализовать программу из Task2  про помощи цикла  while*/
 
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        if ((a == b) || (a > b)){
+            System.out.println("некорректный ввод");
+        }
+        else {
+            int i = a;
+            while ( i < b - 1){
+                i++;
+                if (((i%5) == 0) && ((i%10)!=0)){
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 }
