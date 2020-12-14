82ab923e4c9d3c079c2dc9ea8638ce1e6a902329
devastator1979 <brestdevastator@gmail.com>
2020-12-14 21:19:56 +0300

    Day 2 Task 4

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        in.close();
+        if ((x >= 5) || ((-3 < x) && (x < 5 ))){
+            if (x >= 5){
+                double y = (x*x - 10) / (x +7);
+                System.out.println(y);
+            }
+            if ((-3 < x) && (x < 5 )){
+                double y = (x + 3) * (x * x - 2);
+                System.out.println(y);
+            }
+        }
+        else {
+            System.out.println(420D);
+        }
     }
 }
